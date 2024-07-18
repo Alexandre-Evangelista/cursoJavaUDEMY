d6e22f8bfbccbaf5bb5e566a73ed6a3237290efb
Alexandre Gonçalves Evangelista <140849154+Alexandre-Evangelista@users.noreply.github.com>
2024-07-18 15:56:10 -0300

    adicionando questao lista

---
+++ POO/exerLista/src/Fucionario.java
@@ -0,0 +1,49 @@
+class Funcionario {
+    private Integer id;
+    private String nome;
+    private double salario;
+
+    public Funcionario(Integer id, String nome, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    public void aumentarSalario(double porcentagem) {
+        this.salario+= salario*porcentagem/100;
+
+    }
+}
--- POO/exerLista/src/Main.java
+++ POO/exerLista/src/Main.java
@@ -1,10 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-        List<Fucionario>=
+        List<Funcionario> lista = new ArrayList<>();
+        lista.add(new Funcionario(333, "Maria Brown", 4000));
+        lista.add(new Funcionario(536, "Alex Grey", 3000));
+        lista.add(new Funcionario(772, "Bob Green", 5000));
+
+        System.out.println("Lista de funcionários antes do aumento:");
+        for (Funcionario x : lista) {
+            System.out.println(x.toString());
+        }
+        System.out.println("digite o id do fucionario que ira receber o aumneto");
+        int idFucionario= entrada.nextInt();
+
+        /*metodo para fazer o mesmo que a funçao mas com uma linha
+        Funcionario f = lista.stream().filter(x->x.getId()==idFucionario).findFirst().orElse(null);
+        apenas mudar pos por f e lista.get(pos) por f*/
+
+
+        Integer pos= possuiId(lista,idSalario);
+        if (pos==null){
+            System.out.println("esse id nao existe");
+        }else{
+            System.out.println("digite quanto esse fucionario receberar de aumento");
+            double porcento= entrada.nextDouble();
+            lista.get(pos).aumentarSalario(porcento);
+        }
+        System.out.println("Lista de funcionários depois do aumento:");
+        for (Funcionario x : lista) {
+            System.out.println(x.toString());
+        }
+
+
+        entrada.close();
+    }
+    public static Integer possuiId(List<Funcionario> list, int id){
+        for (int i =0 ;i<list.size();i++){
+            if (list.get(i).getId()==id){
+                return i;
+            }
+
+        }return null;
     }
 }
